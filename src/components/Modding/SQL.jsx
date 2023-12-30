7f79f9e3b4c5bd188c5bf544060eb15e34d2624f
ieb <1@wa.vg>
2023-12-30 14:30:50 +0800

    Full SQL Editor

--- src/components/Modding/SQL.jsx
+++ src/components/Modding/SQL.jsx
@@ -1,4 +1,4 @@
-import {Button, Divider, FormControl, MenuItem, Select, TextField, Typography} from "@mui/material";
+import {Autocomplete, Button, Divider, FormControl, Grid, MenuItem, Select, TextField, Typography} from "@mui/material";
 import {DataGrid} from "@mui/x-data-grid";
 import {useSnackbar} from "notistack";
 import * as React from "react";
@@ -13,6 +13,7 @@ export default function DataBrowser() {
 
   const [tables, setTables] = useState([]);
   const [currentTable, setCurrentTable] = useState("Player");
+  const [isSimple, setIsSimple] = useState(true);
 
   const [columns, setColumns] = useState([]);
   const [maxSizes, setMaxSizes] = useState([]);
@@ -24,7 +25,7 @@ export default function DataBrowser() {
       let r = database.exec(stmt);
       if (r.length) {
         let [{ columns, values }] = r;
-        let maxSizes = columns.map(x => 0);
+        let maxSizes = columns.map(x => Math.min(x.length, 10));
         setColumns(columns);
         setValues(values.map(val => {
           let row = {};
@@ -55,6 +56,9 @@ export default function DataBrowser() {
     try {
       let [{ values }] = database.exec("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name ASC");
       setTables(values.map(x => x[0]));
+      setCurrentTable(currentTable);
+      setStmt(`SELECT * FROM ${currentTable}`);
+      exec(`SELECT *, _rowid_ as _rowid_ FROM ${currentTable}`);
     } catch {
 
     }
@@ -67,47 +71,86 @@ export default function DataBrowser() {
   return (
     <div>
       <Typography variant="h5" component="h5">
-        Data Browser
+        SQLite Browser / Editor
       </Typography>
       <Divider variant="fullWidth" sx={{ my: 2 }} />
-      <div>
-        <Typography sx={{ color: "#777", display: "inline-block", verticalAlign: "middle", mr: 3 }}>Browse Table: </Typography>
-        <FormControl variant="standard" sx={{ display: "inline-block", verticalAlign: "middle" }}>
-          <Select
-            component="label"
-            label="Table"
-            value={currentTable}
-            onChange={(e) => {
-              const sql = `SELECT * FROM ${e.target.value}`
-              setCurrentTable(e.target.value);
-              setStmt(sql); exec(sql);
-            }}
-          >
-            {
-              tables.map(t => <MenuItem value={t} key={t}>{t}</MenuItem>)
+      <Grid container spacing={2} alignItems="center">
+        <Grid item flex={1}>
+          <TextField
+            label="Execute SQL Statement Here"
+            value={stmt}
+            variant="standard"
+            fullWidth
+            onChange={
+              (e) => setStmt(e.target.value)
             }
-          </Select>
-        </FormControl>
-      </div>
+          />
+        </Grid>
+        <Grid item>
+          <Button color="warning" variant="contained" sx={{ my: 1 }} onClick={() => {
+            exec(stmt);
+            setIsSimple(false);
+          }}>
+            Execute
+          </Button>
+        </Grid>
+      </Grid>
       <Divider variant="fullWidth" sx={{ my: 2 }} />
-      <TextField
-        label="SQL Statement"
-        value={stmt}
-        variant="standard"
-        fullWidth
-        onChange={
-          (e) => setStmt(e.target.value)
-        }
-      />
-      <Button color="warning" variant="contained" sx={{ my: 1 }} onClick={() => exec(stmt)}>
-        Execute
-      </Button>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item flex={1}>
+          <FormControl variant="standard" sx={{ display: "inline-block", verticalAlign: "middle", width: "100%" }}>
+            <Autocomplete
+              disablePortal
+              options={tables}
+              value={currentTable}
+              sx={{ width: "100%" }}
+              onChange={(e, nv) => {
+                if (nv) {
+                  setCurrentTable(nv);
+                  setStmt(`SELECT * FROM ${nv}`);
+                  exec(`SELECT _rowid_ as _rowid_, * FROM ${nv}`);
+                  setIsSimple(true);
+                }
+              }}
+              renderInput={(params) => <TextField
+                variant="standard"
+                {...params}
+                label="Browse a Table"
+                autoComplete="off"
+              />}
+            />
+          </FormControl>
+        </Grid>
+      </Grid>
       <Divider variant="fullWidth" sx={{ my: 2 }} />
       <DataGrid
-        rows={values.map((x, _idx) => ({id: _idx, ...x}))}
+        key={stmt}
+        onProcessRowUpdateError={e => console.error(e)}
+        processRowUpdate={(newRow, oldRow) => {
+          if (isSimple) {
+            for(const key of columns) {
+              if (key === "id" || key === "_rowid_") continue;
+              if (newRow[key] !== oldRow[key]) {
+                database.exec(`UPDATE ${currentTable} SET ${key} = :value WHERE _rowid_ = :rowid`, {
+                  ":value": newRow[key],
+                  ":rowid": newRow._rowid_,
+                })
+                exec(`SELECT _rowid_ as _rowid_, * FROM ${currentTable}`);
+              }
+            }
+            // refresh();
+            return newRow;
+          }
+          return oldRow;
+        }}
+        rows={values.map((x, _idx) => (
+          {id: x.row ? x.row : _idx, ...x}
+        ))}
         columns={columns.map((x, _idx) => ({
           field: x,
+          headerName: x === "_rowid_" ? "#" : x,
           flex: maxSizes[_idx],
+          editable: isSimple && (x !== "_rowid_"),
         }))}
         density="compact"
         initialState={{
