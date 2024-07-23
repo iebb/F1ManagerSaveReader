5ed53657631a79f1288208ab98375f38e66a02fa
ieb <1@wa.vg>
2024-07-23 22:24:31 +0800

    support for 2024

--- src/components/UI/Blocks/DragBox.jsx
+++ src/components/UI/Blocks/DragBox.jsx
@@ -17,9 +17,11 @@ export default function DragBox() {
           Try our <a href="https://github.com/iebb/F1MSaveApp/releases/">Windows App</a>, which is able to read Steam and Xbox saves directly.
         </Typography>
         <Typography variant="h5" component="h5">
-          Or... drag your F1 Manager 2022/2023 savefile here to get started.
+          Or... drag your F1 Manager 2022/2023/2024 savefile here to get started.
         </Typography>
         <Typography variant="p" component="p" sx={{ mt: 2 }}>
+          F1 Manager 2024: %LOCALAPPDATA%\F1Manager24\Saved\SaveGames
+          <br />
           F1 Manager 2023: %LOCALAPPDATA%\F1Manager23\Saved\SaveGames
           <br />
           F1 Manager 2022: %LOCALAPPDATA%\F1Manager22\Saved\SaveGames
@@ -29,7 +31,7 @@ export default function DragBox() {
           GPSaveConverter
         </a> to convert the savefile into original format, or use the Windows App.
           <br />
-          Support for F1 Manager 2022 might be limited.
+          <span style={{ color: "yellow" }}>Support for F1 Manager 2024 might be limited.</span>
         </Typography>
       </div>
     </Container>
